ed21a796032fd5ef37be482568ce0d34239901f3
Luiz Eduardo <luizdudumr@gmail.com>
2020-06-03 11:55:06 -0300

    Addition of query results, with print, and error handling

--- main/main.py
+++ main/main.py
@@ -1,6 +1,9 @@
 from google import google
 import os
+import traceback
 
+
+# from main import main
 print('Informe palavras-chaves para a pesquisa')
 pesquisa = input()
 
@@ -17,16 +20,30 @@ dir_path = os.path.join('./resultados', arquivo)
 if not os.path.exists('./resultados'):
     os.makedirs('./resultados')
 
-if(sites):
-    resultados = google.search('site: {}'.format(sites) + '"{}"'.format(pesquisa), int(numeroPag))
-    with open('{}.csv'.format(dir_path), 'w') as csv_file:
-        for res in resultados:
-            csv_file.write(f"{res.name},{res.link},{res.description}\n")
+pesquisas = 0
 
-else:
-    resultados = google.search('"{}"'.format(pesquisa), int(numeroPag))
-    with open('{}.csv'.format(dir_path), 'w') as csv_file:
-        for res in resultados:
-            csv_file.write(f"{res.name},{res.link},{res.description}\n")
+try:
+    if(sites):
+        resultados = google.search('site: {}'.format(
+            sites) + '"{}"'.format(pesquisa), int(numeroPag))
+        with open('{}.csv'.format(dir_path), 'w') as csv_file:
+            for res in resultados:
+                csv_file.write(
+                    f"{res.name},{res.link},{res.description}\n")
+                pesquisas = pesquisas + 1
 
-            
+    else:
+        resultados = google.search('"{}"'.format(pesquisa), int(numeroPag))
+        with open('{}.csv'.format(dir_path), 'w') as csv_file:
+            for res in resultados:
+                csv_file.write(
+                    f"{res.name},{res.link},{res.description}\n") 
+                pesquisas = pesquisas + 1
+
+    if(resultados):
+        print('\n\n\nPesquisa feita com sucesso.\n Palavras-chaves:' + '"{}"'.format(pesquisa) +
+              '\n Sites: ' + '"{}"'.format(sites) + '\n Nome do arquivo: ' + '"{}"'.format(arquivo) + '\n Resultados obtidos em '+ '"{}"'.format(numeroPag) + ' páginas do Google: ' + '"{}"'.format(pesquisas))
+
+except Exception as err:
+    print(err)
+    print('Nenhum resultado encontrado. Isso pode ter acontecido por algum erro interno durante a execução do código ou por uma pesquisa muito restrita/específica.')
